bbb1ec8a45babc348aee29abbac2f8d7ea123e2f
moderatorman <12193288+moderatorman@users.noreply.github.com>
2021-11-20 15:41:30 -0800

    Add missing servlet

---
+++ src/main/java/net/oldschoolminecraft/window/servlets/bans/RevokeBanServlet.java
@@ -0,0 +1,19 @@
+package net.oldschoolminecraft.window.servlets.bans;
+
+import net.oldschoolminecraft.window.servlets.WindowServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RevokeBanServlet extends WindowServlet
+{
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        super.doGet(request, response);
+
+        response.setContentType("application/json");
+        response.getWriter().print(gson.toJson(new WindowServlet.StandardResponse(false, "Not implemented")));
+    }
+}
